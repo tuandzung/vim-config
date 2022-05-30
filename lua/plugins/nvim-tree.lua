46bb8f0ad8d6385187b3906deed382eb0296153a
Ngo Tuan Dzung <zun1903@gmail.com>
2022-05-30 14:33:27 +0700

    feat: update new version of diffview & nvim-tree

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -1,10 +1,6 @@
 -- following options are the default
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
 
-vim.g.nvim_tree_show_icons = { git = 1, folders = 1, files = 1, folder_arrows = 1 }
-vim.g.nvim_tree_icons = {
-    default = '',
-}
 vim.api.nvim_set_keymap('n', '<Leader>n', ':NvimTreeToggle<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<Leader>r', ':NvimTreeRefresh<CR>', { noremap = true })
 vim.api.nvim_set_keymap('n', '<Leader>f', ':NvimTreeFindFile<CR>', { noremap = true })
@@ -16,5 +12,5 @@ require('nvim-tree').setup({
         open_file = {
             resize_window = true,
         },
-    }
+    },
 })
