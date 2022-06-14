b36952cd4ef6a8c8fcc92728ef2c6227782a531f
Ngo Tuan Dzung <zun1903@gmail.com>
2022-06-14 17:26:14 +0700

    feat: update nvim-tree configs

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -8,6 +8,15 @@ vim.api.nvim_set_keymap('n', '<Leader>f', ':NvimTreeFindFile<CR>', { noremap = t
 -- following options are the default
 -- each of these are documented in `:help nvim-tree.OPTION_NAME`
 require('nvim-tree').setup({
+    view = {
+        hide_root_folder = true,
+    },
+    renderer = {
+        highlight_git = true,
+        icons = {
+            git_placement = 'after',
+        },
+    },
     actions = {
         open_file = {
             resize_window = true,
