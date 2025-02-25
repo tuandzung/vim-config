c0c5ddf5905657cfd043c6fed1468ab322604e59
Ngo Tuan Dzung <tuandzung.ngo@gmail.com>
2025-02-25 14:21:14 +0700

    feat: add codeium

--- lua/core.lua
+++ lua/core.lua
@@ -4,17 +4,17 @@ local vim = vim
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
-  local lazyrepo = "https://github.com/folke/lazy.nvim.git"
-  local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
-  if vim.v.shell_error ~= 0 then
-    vim.api.nvim_echo({
-      { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
-      { out, "WarningMsg" },
-      { "\nPress any key to exit..." },
-    }, true, {})
-    vim.fn.getchar()
-    os.exit(1)
-  end
+    local lazyrepo = "https://github.com/folke/lazy.nvim.git"
+    local out = vim.fn.system({ "git", "clone", "--filter=blob:none", "--branch=stable", lazyrepo, lazypath })
+    if vim.v.shell_error ~= 0 then
+        vim.api.nvim_echo({
+            { "Failed to clone lazy.nvim:\n", "ErrorMsg" },
+            { out,                            "WarningMsg" },
+            { "\nPress any key to exit..." },
+        }, true, {})
+        vim.fn.getchar()
+        os.exit(1)
+    end
 end
 vim.opt.rtp:prepend(lazypath)
 
@@ -143,14 +143,14 @@ require('lazy').setup({
     'ethanholz/nvim-lastplace',
     'williamboman/nvim-lsp-installer',
     { 'TimUntersberger/neogit', dependencies = 'nvim-lua/plenary.nvim' },
-    { 'monaqa/dial.nvim', dependencies = 'nvim-lua/plenary.nvim' },
+    { 'monaqa/dial.nvim',       dependencies = 'nvim-lua/plenary.nvim' },
     { -- Browser
         'glacambre/firenvim',
         build = function()
             vim.fn['firenvim#install'](0)
         end,
     },
-    { 'sindrets/diffview.nvim', dependencies = 'nvim-lua/plenary.nvim' },
+    { 'sindrets/diffview.nvim',      dependencies = 'nvim-lua/plenary.nvim' },
     { 'anuvyklack/pretty-fold.nvim', dependencies = { 'anuvyklack/nvim-keymap-amend' } },
     'Xuyuanp/scrollbar.nvim',
     'onsails/lspkind.nvim',
@@ -165,4 +165,10 @@ require('lazy').setup({
     },
     'rmagatti/goto-preview',
     'windwp/nvim-spectre',
+    {
+        'Exafunction/codeium.vim',
+        name = 'codeium',
+        build = ':Codeium Auth',
+        dependencies = {'nvim-lua/plenary.nvim'}
+    },
 })
