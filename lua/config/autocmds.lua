5b454d4b0922e7a43da212f7ab7a8003e4e3cb92
Ngo Tuan Dzung <zun1903@gmail.com>
2025-03-22 22:52:48 +0700

    feat: added autocmds for auto-cursorline

---
+++ lua/config/autocmds.lua
@@ -0,0 +1,20 @@
+vim.api.nvim_create_autocmd({ 'InsertLeave', 'WinEnter' }, {
+  group = vim.api.nvim_create_augroup('cursor_active_window', {}),
+  callback = function()
+    local ok, cl = pcall(vim.api.nvim_win_get_var, 0, 'auto-cursorline')
+    if ok and cl then
+      vim.wo.cursorline = true
+      vim.api.nvim_win_del_var(0, 'auto-cursorline')
+    end
+  end,
+})
+vim.api.nvim_create_autocmd({ 'InsertEnter', 'WinLeave' }, {
+  group = vim.api.nvim_create_augroup('cursor_inactive_window', {}),
+  callback = function()
+    local cl = vim.wo.cursorline
+    if cl then
+      vim.api.nvim_win_set_var(0, 'auto-cursorline', cl)
+      vim.wo.cursorline = false
+    end
+  end,
+})
