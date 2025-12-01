5eb4ba12b6f978f82e51b859df948cb3930d678b
Ngo Tuan Dzung <mail@tuandzung.me>
2025-12-01 16:47:52 +0700

    feat: update plugins and added sidekick

--- lua/plugins/editor/ai.lua
+++ lua/plugins/editor/ai.lua
@@ -1,18 +1,11 @@
-local config = require('config.ai')
-local current_model = config.available_models[1] -- Consider if you need this variable anymore or how it should be used
-
-local function select_model()
-  vim.ui.select(config.available_models, {
-    prompt = 'Select Model:',
-  }, function(choice)
-    if choice then
-      current_model = choice -- Update the model if needed elsewhere
-      vim.notify('Selected model: ' .. current_model)
-    end
-  end)
-end
-
 return {
+  {
+    'Davidyz/VectorCode', -- Index and search code in your repositories
+    version = '*',
+    build = 'uv tool upgrade vectorcode',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    cmd = 'VectorCode',
+  },
   -- Supermaven
   { import = 'lazyvim.plugins.extras.ai.supermaven' },
   {
@@ -31,152 +24,93 @@ return {
     },
     init = function()
       _G.completion_sources = vim.tbl_extend('force', _G.completion_sources, {
-        supermaven = '「AI」',
+        supermaven = '「Supermaven」',
+      })
+    end,
+  },
+  -- Minuet
+  {
+    'milanglacier/minuet-ai.nvim',
+    config = function()
+      require('vectorcode').setup({
+        -- number of retrieved documents
+        n_query = 1,
+      })
+      local has_vc, vectorcode_config = pcall(require, 'vectorcode.config')
+      local vectorcode_cacher = nil
+      if has_vc then
+        vectorcode_cacher = vectorcode_config.get_cacher_backend()
+      end
+
+      -- roughly equate to 2000 tokens for LLM
+      local RAG_Context_Window_Size = 8000
+
+      require('minuet').setup({
+        provider = 'openai_fim_compatible',
+        n_completions = 1, -- recommend for local model for resource saving
+        -- I recommend beginning with a small context window size and incrementally
+        -- expanding it, depending on your local computing power. A context window
+        -- of 512, serves as an good starting point to estimate your computing
+        -- power. Once you have a reliable estimate of your local computing power,
+        -- you should adjust the context window to a larger value.
+        context_window = 65536,
+        provider_options = {
+          openai_fim_compatible = {
+            -- For Windows users, TERM may not be present in environment variables.
+            -- Consider using APPDATA instead.
+            api_key = 'LLAMA_API_KEY',
+            name = 'llama.cpp',
+            end_point = 'http://192.168.2.32:8001/v1/completions',
+            model = 'PLACEHOLDER',
+            -- Llama.cpp does not support the `suffix` option in FIM completion.
+            -- Therefore, we must disable it and manually populate the special
+            -- tokens required for FIM completion.
+            template = {
+              prompt = function(
+                context_before_cursor,
+                context_after_cursor,
+                _
+              )
+                return '<|fim_prefix|>'
+                  .. context_before_cursor
+                  .. '<|fim_suffix|>'
+                  .. context_after_cursor
+                  .. '<|fim_middle|>'
+              end,
+              suffix = false,
+            },
+          },
+        },
+      })
+    end,
+    init = function()
+      _G.completion_sources = vim.tbl_extend('force', _G.completion_sources, {
+        minuet = '「Minuet」',
       })
     end,
   },
   {
-    -- AI Chat
-    'olimorris/codecompanion.nvim',
-    dependencies = {
-      'j-hui/fidget.nvim',
-      {
-        'ravitemer/mcphub.nvim', -- Manage MCP servers
-        cmd = 'MCPHub',
-        build = 'npm install -g mcp-hub@latest',
-        config = true,
+    'saghen/blink.cmp',
+    optional = true,
+    opts = {
+      keymap = {
+        ['<A-y>'] = {
+          function(cmp) cmp.show({ providers = { 'minuet' } }) end,
+        },
       },
-      {
-        'Davidyz/VectorCode', -- Index and search code in your repositories
-        version = '*',
-        build = 'uv tool install vectorcode',
-        dependencies = { 'nvim-lua/plenary.nvim' },
+      sources = {
+        -- if you want to use auto-complete
+        default = { 'minuet' },
+        providers = {
+          minuet = {
+            name = 'minuet',
+            module = 'minuet.blink',
+            score_offset = 100,
+          },
+        },
       },
     },
-    cmd = { 'CodeCompanion', 'CodeCompanionChat' },
-    keys = {
-      -- Define the keymap here using the 'keys' table
-      {
-        '<leader>og',
-        select_model, -- Use the function defined above
-        desc = 'Select OpenRouter Model',
-        mode = 'n', -- Specify the mode
-      },
-      -- You could potentially add more keys here that should load the plugin
-      {
-        '<C-a>',
-        '<cmd>CodeCompanionActions<CR>',
-        desc = 'Open the action palette',
-        mode = { 'n', 'v' },
-      },
-      {
-        '<Leader>a',
-        '<cmd>CodeCompanionChat Toggle<CR>',
-        desc = 'Toggle a chat buffer',
-        mode = { 'n', 'v' },
-      },
-      {
-        '<LocalLeader>a',
-        '<cmd>CodeCompanionChat Add<CR>',
-        desc = 'Add code to a chat buffer',
-        mode = { 'v' },
-      },
-    },
-    init = function()
-      _G.completion_sources = vim.tbl_extend('force', _G.completion_sources, {
-        CodeCompanion = '「AI」',
-      })
-      require('plugins.others.spinner'):init()
-    end,
-    config = function()
-      require('codecompanion').setup({
-        extensions = {
-          mcphub = {
-            callback = function()
-              return require('mcphub.extensions.codecompanion')
-            end,
-            opts = {
-              make_vars = true,
-              make_slash_commands = true,
-              show_result_in_chat = true,
-            },
-          },
-          vectorcode = {
-            opts = {
-              add_tool = true,
-            },
-          },
-        },
-        adapters = {
-          openrouter = function()
-            return require('codecompanion.adapters').extend(
-              'openai_compatible',
-              {
-                env = {
-                  url = 'https://openrouter.ai/api',
-                  api_key = 'cmd: rbw get OpenRouter',
-                  chat_url = '/v1/chat/completions',
-                },
-                schema = {
-                  model = {
-                    default = current_model,
-                  },
-                },
-              }
-            )
-          end,
-        },
-        strategies = {
-          chat = {
-            roles = {
-              user = 'tuandzung',
-            },
-            adapter = vim.env.VIM_COMPANION_ADAPTER
-              or config.chat_adapter
-              or 'ollama',
-            tools = {
-              opts = {
-                auto_submit_success = false,
-                auto_submit_errors = false,
-              },
-            },
-          },
-          inline = {
-            adapter = vim.env.VIM_COMPANION_ADAPTER
-              or config.inline_adapter
-              or 'ollama',
-          },
-        },
-        slash_commands = {
-          ['buffer'] = {
-            opts = {
-              keymaps = {
-                modes = {
-                  i = '<C-b>',
-                },
-              },
-            },
-          },
-          ['help'] = {
-            opts = {
-              max_lines = 1000,
-            },
-          },
-        },
-        display = {
-          action_palette = {
-            provider = 'default',
-          },
-          chat = {
-            show_references = true,
-            show_header_separator = false,
-            show_settings = false,
-          },
-          diff = {
-            provider = 'mini_diff',
-          },
-        },
-      })
-    end,
   },
+  -- AI CLI
+  { import = 'lazyvim.plugins.extras.ai.sidekick' },
 }
