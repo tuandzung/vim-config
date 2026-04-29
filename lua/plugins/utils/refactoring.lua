d70d8ce2dfd5c553e12c327beada761f3847983f
Ngo Tuan Dzung <mail@tuandzung.me>
2026-04-29 10:30:54 +0700

    feat: update plugins version and add async dep for refactoring.nvim

--- lua/plugins/utils/refactoring.lua
+++ lua/plugins/utils/refactoring.lua
@@ -1,4 +1,11 @@
 return {
   -- Refactoring
   { import = 'lazyvim.plugins.extras.editor.refactoring' },
+  {
+    'ThePrimeagen/refactoring.nvim',
+    dependencies = {
+      'lewis6991/async.nvim',
+    },
+    lazy = false,
+  },
 }
