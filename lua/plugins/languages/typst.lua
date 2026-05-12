40125024891fff1f2b38762bd7f920fd49f66b6d
Ngo Tuan Dzung <mail@tuandzung.me>
2026-05-13 00:55:56 +0700

    feat: added typst support

---
+++ lua/plugins/languages/typst.lua
@@ -0,0 +1,60 @@
+local language = require('config.languages').typst
+
+return {
+  {
+    -- LSP config
+    'neovim/nvim-lspconfig',
+    opts = {
+      servers = {
+        tinymist = {
+          keys = {
+            {
+              '<leader>cP',
+              function()
+                local buf_name = vim.api.nvim_buf_get_name(0)
+                LazyVim.lsp.execute({
+                  command = 'tinymist.pinMain',
+                  arguments = { buf_name },
+                })
+              end,
+              ft = language.filetypes,
+              desc = 'Pin main file',
+            },
+          },
+          single_file_support = true, -- Fixes LSP attachment in non-Git directories
+          settings = {
+            formatterMode = 'typstyle',
+          },
+        },
+      },
+    },
+  },
+  {
+    -- Typst preview
+    'chomosuke/typst-preview.nvim',
+    ft = language.filetypes,
+    cmd = { 'TypstPreview', 'TypstPreviewToggle', 'TypstPreviewUpdate' },
+    keys = {
+      {
+        '<leader>cp',
+        ft = language.filetypes,
+        '<cmd>TypstPreviewToggle<cr>',
+        desc = 'Toggle Typst Preview',
+      },
+    },
+    opts = {
+      dependencies_bin = {
+        tinymist = 'tinymist',
+      },
+    },
+  },
+  {
+    -- Comment
+    'folke/ts-comments.nvim',
+    opts = {
+      lang = {
+        typst = { '// %s', '/* %s */' },
+      },
+    },
+  },
+} or {}
